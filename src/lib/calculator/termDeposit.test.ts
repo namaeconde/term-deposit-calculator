d982ce61c1d6967ffb0471444384b058968fb77c
Namae Conde <namaeconde@gmail.com>
2025-07-17 23:32:43 +1000

    Add compound balance calculator, unit test scenarios and mocks

--- src/lib/calculator/termDeposit.test.ts
+++ src/lib/calculator/termDeposit.test.ts
@@ -1,20 +1,27 @@
 import { describe, expect, test } from '@jest/globals';
-import { calculateSimpleBalance } from './termDeposit';
+import { calculateCompoundBalance, calculateSimpleBalance } from './termDeposit';
 import { TermDepositInput } from "./types";
 import {
     MOCK_1K_DEPOSIT_MONTH_AT_MATURITY,
-    MOCK_1K_DEPOSIT_YEAR_ANNUALLY, MOCK_1K_DEPOSIT_YEAR_AT_MATURITY,
+    MOCK_1K_DEPOSIT_YEAR_ANNUALLY,
+    MOCK_1K_DEPOSIT_YEAR_AT_MATURITY,
+    MOCK_1K_DEPOSIT_YEAR_MONTHLY,
+    MOCK_1K_DEPOSIT_YEAR_QUARTERLY,
+    MOCK_25K_DEPOSIT_MONTH_ANNUALLY,
+    MOCK_25K_DEPOSIT_MONTH_MONTHLY,
+    MOCK_25K_DEPOSIT_MONTH_QUARTERLY,
 } from "./__mocks__/SimpleDepositMock";
 
 describe("calculateSimpleBalance", () => {
     const testCases = [
-        [MOCK_1K_DEPOSIT_YEAR_AT_MATURITY, 1033], // [input, balance]
-        [MOCK_1K_DEPOSIT_MONTH_AT_MATURITY, 1003]
+        // [scenario, input, balance]
+        ["1k deposit at maturity for 3 years", MOCK_1K_DEPOSIT_YEAR_AT_MATURITY, 1033],
+        ["1k deposit at maturity for 3 months", MOCK_1K_DEPOSIT_MONTH_AT_MATURITY, 1003]
     ]
 
     test.each(testCases)(
-        'should return simple balance',
-        (input, balance) => {
+        'should return simple balance for %s',
+        (_, input, balance) => {
             expect(calculateSimpleBalance(input as TermDepositInput)).toEqual(balance);
         }
     )
@@ -27,5 +34,34 @@ describe("calculateSimpleBalance", () => {
         // THEN
         expect(() =>  calculateSimpleBalance(input))
             .toThrow("Invalid Interest Paid Frequency, expected AT_MATURITY value.");
+    });
+});
+
+describe("calculateCompoundBalance", () => {
+    const testCases = [
+        // [scenario, input, balance]
+        ["1k deposit monthly for 3 years", MOCK_1K_DEPOSIT_YEAR_MONTHLY, 1034],
+        ["1k deposit quarterly for 3 years", MOCK_1K_DEPOSIT_YEAR_QUARTERLY, 1034],
+        ["1k deposit annually for 3 years", MOCK_1K_DEPOSIT_YEAR_ANNUALLY, 1033],
+        ["1k deposit monthly for 3 months", MOCK_25K_DEPOSIT_MONTH_MONTHLY, 25069],
+        ["1k deposit quarterly for 3 months", MOCK_25K_DEPOSIT_MONTH_QUARTERLY, 25069],
+        ["1k deposit annually for 3 months", MOCK_25K_DEPOSIT_MONTH_ANNUALLY, 25068],
+    ]
+
+    test.each(testCases)(
+        'should return compounding balance for %s',
+        (_, input, balance) => {
+            expect(calculateCompoundBalance(input as TermDepositInput)).toEqual(balance);
+        }
+    )
+
+    test('should throw error if interestPaidFrequency is not AT_MATURITY', () => {
+        // GIVEN
+        const input = MOCK_1K_DEPOSIT_MONTH_AT_MATURITY;
+
+        // WHEN
+        // THEN
+        expect(() =>  calculateCompoundBalance(input))
+            .toThrow("Invalid Interest Paid Frequency, expected to value: MONTHLY, QUARTERLY or ANNUALLY.");
     });
 })
--- src/lib/calculator/termDeposit.ts
+++ src/lib/calculator/termDeposit.ts
@@ -1,4 +1,4 @@
-import { InterestPaidFrequency, TermDepositInput } from "./types";
+import { CompoundingFactor, InterestPaidFrequency, TermDepositInput } from "./types";
 import { convertInterestRateInDecimal, convertInvestmentTermInYears } from "../../utils/converters";
 
 /**
@@ -17,5 +17,34 @@ export function calculateSimpleBalance({ depositAmount, interestRate, investment
     const investmentTermInYears = convertInvestmentTermInYears(investmentTerm, investmentTermFormat);
     const interestInDecimal = convertInterestRateInDecimal(interestRate);
 
-    return Math.round(depositAmount * (1 + (interestInDecimal * investmentTermInYears)));
+    const balance = depositAmount * (1 + (interestInDecimal * investmentTermInYears));
+    return Math.round(balance);
+}
+
+/**
+ * Calculates balance for compounding interest paid using formula rounded to the nearest value,
+ * where:
+ * compoundingFactor = 12 if interestPaidFrequency is "monthly"
+ * compoundingFactor = 4 if interestPaidFrequency is "quarterly"
+ * compoundingFactor = 1 if interestPaidFrequency is "annually"
+ *
+ * balance = depositAmount * ((1 + (interestRate/100) / compoundingFactor)^(compoundingFactor * investmentTerm))
+ *
+ * @param input
+ * @returns balance
+ */
+export function calculateCompoundBalance({ depositAmount, interestRate, investmentTerm, investmentTermFormat,
+                                             interestPaidFrequency }: TermDepositInput) {
+    if (interestPaidFrequency === InterestPaidFrequency.AT_MATURITY) {
+        throw new Error("Invalid Interest Paid Frequency, expected to value: MONTHLY, QUARTERLY or ANNUALLY.");
+    }
+
+    let compoundingFactor = CompoundingFactor.ANNUALLY;
+    if (interestPaidFrequency === InterestPaidFrequency.MONTHLY) compoundingFactor = CompoundingFactor.MONTHLY;
+    if (interestPaidFrequency === InterestPaidFrequency.QUARTERLY) compoundingFactor = CompoundingFactor.QUARTERLY;
+
+    const investmentTermInYears = convertInvestmentTermInYears(investmentTerm, investmentTermFormat);
+    const interestInDecimal = convertInterestRateInDecimal(interestRate);
+    const balance = depositAmount * ((1 + interestInDecimal/compoundingFactor)**(compoundingFactor*investmentTermInYears));
+    return Math.round(balance);
 }
